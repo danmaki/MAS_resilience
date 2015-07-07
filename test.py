d99f566b6f009dd9d60d555d924ddaa4bcaf8792
danmaki <maki.dan@keio.jp>
2015-07-07 14:10:42 +0900

    2015/07/07

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # インポート
+import os
 import random
+import datetime
 import numpy as np
 import scipy as sp
 import pandas as pd
@@ -9,8 +11,43 @@ import matplotlib.pyplot as plt
 import math
 import wx
 
+# クラス定義
+class Agent:    #エージェントクラス
+    def __init__(self,Number,Type,SC,ST):
+        self.Number = Number
+        self.Type = Type
+        self.SC = SC
+        self.ST = ST
+    def location(self,x,y):
+        locationData = {}
+        locationData['agentNumber'] = self.Number
+        locationData['locationX'] = x
+        locationData['locationY'] = y
+        return locationData
+    def newSC(self,ST):
+        self.SC = self.SC + ST
+        if self.SC<0:
+            self.SC = 0
+
+class SeismicLoss:
+    def __init__(self,agent,loss):
+        self.agent = agent
+        self.loss = loss
+    def lossCalc(self):
+        return loss
+
+# 関数定義
+def savefigaspdf(name):
+    date = datetime.date.today()
+    if os.path.exists("/Users/danmaki/GoogleDrive/Program/figures/resilience/%s-%s-%s"% (date.year, date.month, date.day))==False:
+        os.mkdir("/Users/danmaki/GoogleDrive/Program/figures/resilience/%s-%s-%s"\
+        % (date.year, date.month, date.day))
+    plt.savefig("/Users/danmaki/GoogleDrive/Program/figures/resilience/%s-%s-%s/%s.pdf"\
+    % (date.year, date.month, date.day, name))
+    plt.close
+
+
 if __name__ == "__main__":
-    marks = ['club', 'diamond', 'heart', 'spade']
-    numbers = range(1, 14)
-    cards = [(m, n) for m in marks for n in numbers]
-    print cards
+    a = Agent(1,"human",200,random.randint(0,10))
+    locationData = a.location(1,10)
+    print locationData
